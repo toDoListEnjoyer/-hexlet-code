674b394d923fa57eb2232d1cf531115fd6e9f725
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2022-12-01 13:59:23 +0300

    add brain-gcd.js

---
+++ bin/brain-gcd.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import {
+  cons, car, cdr,
+} from '@hexlet/pairs';
+import {
+  numberOfRounds, maxNumberToGenerate, greeting,
+  getAnswerToQuestion, checkAnswer, printEndGameMessage,
+} from '../src/index.js';
+import askForName from '../src/cli.js';
+
+let rounds = numberOfRounds;
+const max = maxNumberToGenerate;
+
+const gcd = (a, b) => {
+  const remainder = a % b;
+  if (remainder !== 0) return gcd(b, remainder);
+  return b;
+};
+
+greeting();
+const name = askForName();
+console.log('Find the greatest common divisor of given numbers.');
+
+while (rounds) {
+  let pair = cons(Math.floor(Math.random() * max + 1), Math.floor(Math.random() * max + 1));
+  const question = `Question: ${car(pair)} ${cdr(pair)}`;
+  const answer = getAnswerToQuestion(question);
+
+  if (!parseInt(answer, 10)) break; // Check for correct input
+
+  if (car(pair) < cdr(pair)) pair = cons(cdr(pair), car(pair));
+  const expectedAnswer = gcd(car(pair), cdr(pair));
+  if (!checkAnswer(answer, expectedAnswer.toString(), name)) break;
+
+  rounds -= 1;
+}
+
+printEndGameMessage(name, rounds);
